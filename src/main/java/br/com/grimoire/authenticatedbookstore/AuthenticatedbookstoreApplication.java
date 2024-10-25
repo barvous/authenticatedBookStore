448e649b614f1da6a8ebeedab60008bb536f66b8
Marcos Daniel <marcostkd20147@gmail.com>
2024-10-24 22:05:09 -0300

    Project created

---
+++ src/main/java/br/com/grimoire/authenticatedbookstore/AuthenticatedbookstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.grimoire.authenticatedbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatedbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatedbookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/grimoire/authenticatedbookstore/AuthenticatedbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grimoire.authenticatedbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatedbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
